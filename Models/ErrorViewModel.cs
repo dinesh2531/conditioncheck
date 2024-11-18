ad6de045f72418dc0acb9cbade3b1dd4ed516d8d
dinesh.s <dinesh.s@changepond.com>
2024-11-18 19:30:10 +0530

    1st commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Sonarlint.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
